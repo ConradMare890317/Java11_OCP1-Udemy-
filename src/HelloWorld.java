ee7033fe243e2c489d707abca075883a46d5f331
ConradMare890317 <Conradmare@gmail.com>
2021-08-27 13:52:40 +0200

    2021/08/27 (pm)

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,8 @@
 public class HelloWorld {
-    public static void main(String[] args)
+    public static void main(String[] args) {
         System.out.println("Hello World!");
-        }
+
+        int myFirstNumber = 10;
+        System.out.println(myFirstNumber);
+    }
+}
