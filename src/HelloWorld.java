f148667d00dbfa91311688b0e1e66916e487c053
ConradMare890317 <Conradmare@gmail.com>
2021-08-27 14:11:48 +0200

    2021/08/27 (pm) Getting there.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        int myFirstNumber = 10;
+        int myFirstNumber = 10 + 5;
         System.out.println(myFirstNumber);
     }
 }
